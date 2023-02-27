9f6cf7fb4fa09e5766de700e0c6924e47a340326
Ori Amor <oriamor50@gmail.com>
2023-02-27 11:49:25 +0200

    added lines

--- sqrt of complex number.py
+++ sqrt of complex number.py
@@ -6,4 +6,10 @@ a1=y/(2*math.sqrt((-x+math.sqrt(x**2+y**2))/2))
 b1=math.sqrt((-x+math.sqrt(x**2+y**2))/2)
 a2=-y/(2*math.sqrt((-x+math.sqrt(x**2+y**2))/2))
 b2=-math.sqrt((-x+math.sqrt(x**2+y**2))/2)
-print(f'the square root of the complex number {x}+{y}i is {a1}+{b1}i or {a2}+{b2}i')
+if x==0:
+    print(f'the square root of the complex number {y}i is {a1}+{b1}i or {a2}+{b2}i')
+elif y==0:
+    print(f'the square root of the complex number {x} is {b1}i or {b2}i')
+else:
+    print(f'the square root of the complex number {x}+{y}i is {a1}+{b1}i or {a2}+{b2}i')
+print(a1, a2)
