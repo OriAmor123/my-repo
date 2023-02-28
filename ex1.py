0968fa30669c4094260d1456d27188b3de56818d
Ori Amor <oriamor50@gmail.com>
2023-02-28 21:31:28 +0200

    ח

--- ex1.py
+++ ex1.py
@@ -3,11 +3,11 @@ num = input('insect a number: ')
 counter = 0 #the count on numbers inserted
 average = 0
 while num !='q':
-    if not num.isnumeric():
+    if not num.isnumeric(): #checks if the input is not a number and continue
         num = input('insect a number: ')
         continue
     else:
-        average = (average*counter + float(num))/(counter+1) #doesn't work properly
+        average = (average*counter + float(num))/(counter+1) #checks if the input is a number and adding the number to the average
         counter +=1
     num = input('insect a number: ')
 
