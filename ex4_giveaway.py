10fe729e281dc01b757481f90237d01252e5df3a
Ori Amor <oriamor50@gmail.com>
2023-03-09 10:25:46 +0200

    added condition

--- ex4_giveaway.py
+++ ex4_giveaway.py
@@ -1,10 +1,14 @@
 num1 = int(input('insert a first number between 1-9: '))
 num2 = int(input('insert a second number between 1-9: '))
 
+#making sure that there is no duplicate guesses
+while num2 == num1:
+    print('you can\'t insert the same number twice')
+    num2 = int(input('insert a second number between 1-9: '))
+
 #choosing 2 numbers between 1-9
-list_of_nums = [1,2,3,4,5,6,7,8,9]
 import random
-rd_from_list = random.sample(list_of_nums, 2)
+rd_from_list = random.sample(range(1,10), 2)
 
 #checks if the player guessed correctly
 if num1 == rd_from_list[0] or num1==rd_from_list[1] or num2==rd_from_list[0] or num2==rd_from_list[1]:
