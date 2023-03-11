4c235d1e8693728aaacf9a17d8b4976486f112be
Ori Amor <oriamor50@gmail.com>
2023-03-12 00:27:05 +0200

    round

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -5,11 +5,11 @@ C=float(input('Please enter C: '))
 D = -B/(2*A)
 E=A*D**2 +B*D + C
 print(f'f(x)={A}x\u00b2+{B}x+{C}=0')
-print(f'vertex:({D},{E})')
+print(f'vertex:({round(D,2)},{round(E,2)})')
 if B**2 - 4*A*C >0:
-    print(f'x1={(-B + math.sqrt(B**2 - 4*A*C))/(2*A)}')
-    print(f'x2={(-B - math.sqrt(B**2 - 4*A*C))/(2*A)}')
+    print(f'x1={round((-B + math.sqrt(B**2 - 4*A*C))/(2*A),2)}')
+    print(f'x2={round((-B - math.sqrt(B**2 - 4*A*C))/(2*A),2)}')
 elif B**2-4*A*C==0:
-    print(f'x={-B/(2*A)}')
+    print(f'x={round(-B/(2*A),2)}')
 else:
     print('There is no intersections with X axis')
